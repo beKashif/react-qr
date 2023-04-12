6a348ad71e19b9204e84bf355dfd508a708ec1f1
Kashif Shehzad Baloch <87254179+beKashif@users.noreply.github.com>
2023-04-12 16:34:33 +0500

    Update QrCodeGen.jsx

--- src/components/QrCodeGen.jsx
+++ src/components/QrCodeGen.jsx
@@ -1,11 +1,13 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import './QrCodeGen.css'
 import QRCode from 'qrcode.react'
+import ReactDOMServer from 'react-dom/server'
 
 const QrCodeGen = () => {
 
     const [data, setData] = useState('');
     const [qrCodeVisible, setQrCodeVisible] = useState(false);
+    const qrCodeRef = useRef(null);
 
     const handleInputChange = (event) => {
         setData(event.target.value);
@@ -15,6 +17,19 @@ const QrCodeGen = () => {
     const handleGenerateQRCode = (event) => {
         event.preventDefault();
         setQrCodeVisible(true)
+    }
+
+    const handleDownloadQRCode = (event) => {
+        event.preventDefault();
+        const svgString = ReactDOMServer.renderToStaticMarkup(qrCodeRef.current);
+        const blob = new Blob([svgString], {type: 'image/svg+xml'});
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.download = 'qrcode.svg';
+        link.href = url;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
     }
 
   return (
@@ -39,7 +54,17 @@ const QrCodeGen = () => {
                     Generate
                 </button>
 
-                {qrCodeVisible && data && <QRCode value={data} className="qr-code" />}
+                {qrCodeVisible && data && <QRCode value={data} className="qr-code" ref={qrCodeRef} />}
+                
+                {qrCodeVisible && data && 
+                    <button
+                    type='button'
+                    className='qr-download-btn'
+                    onClick={handleDownloadQRCode}
+                    >
+                        Download SVG
+                    </button>
+                }
             </form>
         </div>
     </div>
