ff85750428ba7209ea6fba2006d5af987edbb224
Kashif Shehzad Baloch <87254179+beKashif@users.noreply.github.com>
2023-04-12 16:55:19 +0500

    Update QrCodeGen.jsx

--- src/components/QrCodeGen.jsx
+++ src/components/QrCodeGen.jsx
@@ -1,86 +1,87 @@
-import React, {useState, useRef} from 'react'
-import './QrCodeGen.css'
-import QRCode from 'qrcode.react'
-import ReactDOMServer from 'react-dom/server'
+import React, {useState, useRef} from 'react';
+import './QrCodeGen.css';
+import QRCode from 'qrcode.react';
+import ReactDOMServer from 'react-dom/server';
 
 const QrCodeGen = () => {
+  const [data, setData] = useState('');
+  const [qrCodeVisible, setQrCodeVisible] = useState(false);
+  const qrCodeRef = useRef(null);
 
-    const [data, setData] = useState('');
-    const [qrCodeVisible, setQrCodeVisible] = useState(false);
-    const qrCodeRef = useRef(null);
-
-    const handleInputChange = (event) => {
-        setData(event.target.value);
-        setQrCodeVisible(false);
-    }
-
-    const handleGenerateQRCode = (event) => {
-        event.preventDefault();
-        setQrCodeVisible(true)
-    }
-
-    const handleDownloadQRCode = (event) => {
-  event.preventDefault();
-  const svgString = ReactDOMServer.renderToStaticMarkup(qrCodeRef.current);
-  const canvas = document.createElement("canvas");
-  const svg = new Blob([svgString], {type: "image/svg+xml;charset=utf-8"});
-  const url = URL.createObjectURL(svg);
-  const image = new Image();
-  image.onload = () => {
-    canvas.width = image.width;
-    canvas.height = image.height;
-    const context = canvas.getContext("2d");
-    context.drawImage(image, 0, 0);
-    const png = canvas.toDataURL("image/png");
-    const link = document.createElement("a");
-    link.download = "qrcode.png";
-    link.href = png;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  const handleInputChange = (event) => {
+    setData(event.target.value);
+    setQrCodeVisible(false);
   };
-  image.src = url;
-};
 
+  const handleGenerateQRCode = (event) => {
+    event.preventDefault();
+    setQrCodeVisible(true);
+  };
+
+  const handleDownloadQRCode = (event) => {
+    event.preventDefault();
+    const svgString = ReactDOMServer.renderToStaticMarkup(qrCodeRef.current);
+    const canvas = document.createElement('canvas');
+    const svg = new Blob([svgString], {type: 'image/svg+xml;charset=utf-8'});
+    const url = URL.createObjectURL(svg);
+    const image = new Image();
+    image.onload = () => {
+      canvas.width = image.width;
+      canvas.height = image.height;
+      const context = canvas.getContext('2d');
+      context.drawImage(image, 0, 0);
+      const png = canvas.toDataURL('image/png');
+      const link = document.createElement('a');
+      link.download = 'qrcode.png';
+      link.href = png;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    };
+    image.src = url;
+  };
 
   return (
     <>
-    <div className='appbody'>
-        <div className="qr-container">
-            <h1><span className='react'>React</span> Qr-Code Generator</h1>
+      <div className='appbody'>
+        <div className='qr-container'>
+          <h1>
+            <span className='react'>React</span> Qr-Code Generator
+          </h1>
 
-            <form className='qr-form'>
-                <input 
-                type="text"
-                className='qr-input'
-                value={data}
-                onChange={handleInputChange}
-                placeholder="Enter data"
-                />
+          <form className='qr-form'>
+            <input
+              type='text'
+              className='qr-input'
+              value={data}
+              onChange={handleInputChange}
+              placeholder='Enter data'
+            />
+            <button
+              type='submit'
+              className='qr-btn'
+              onClick={handleGenerateQRCode}
+            >
+              Generate
+            </button>
+
+            {qrCodeVisible && data && (
+              <>
+                <QRCode value={data} className='qr-code' ref={qrCodeRef} />
                 <button
-                type='submit'
-                className='qr-btn'
-                onClick={handleGenerateQRCode}
+                  type='button'
+                  className='qr-download-btn'
+                  onClick={handleDownloadQRCode}
                 >
-                    Generate
+                  Download as PNG
                 </button>
-
-                {qrCodeVisible && data && <QRCode value={data} className="qr-code" ref={qrCodeRef} />}
-                
-                {qrCodeVisible && data && 
-                    <button
-                    type='button'
-                    className='qr-btn'
-                    onClick={handleDownloadQRCode}
-                    >
-                        Download Png
-                    </button>
-                }
-            </form>
+              </>
+            )}
+          </form>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default QrCodeGen
+export default QrCodeGen;
